2f75ad1b3d790309e527a916a4cfaaca6e42abf2
Michael Watts <wattsjmichael@gmail.com>
2021-04-11 10:50:41 -0700

    Creates room with interactable objects

---
+++ Assets/Scripts/RotatorObject.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorObject : MonoBehaviour
+{
+
+    [Range(-1.0f, 1.0f)]
+    public float xForceDirection = 0.0f;
+    [Range(-1.0f, 1.0f)]
+    public float yForceDirection = 0.0f;
+    [Range(-1.0f, 1.0f)]
+    public float zForceDirection = 0.0f;
+    public float speedMultiplier = 1;
+    public bool worldPivote = false;
+    
+    private Space spacePivot = Space.Self;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (worldPivote) spacePivot = Space.World;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       this.transform.Rotate(xForceDirection * speedMultiplier, yForceDirection * speedMultiplier, zForceDirection * speedMultiplier); 
+    }
+}
